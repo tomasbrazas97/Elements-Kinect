567c6045591af0ccd94b1d08f1edfc80b64f5c2d
tomasbrazas97 <tomasbrazas@gmail.com>
2020-04-07 01:35:46 +0100

    orb collision
    minor bug does not destroy like supposed to

---
+++ Elements/Assets/KinectView/Scripts/HandMockUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMockUp : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "PR_Orb")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
